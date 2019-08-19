9f1605a7a5117d9ec070b8da987837095d89f0e7
Muuyi Andrew <muuyiandrew2015@gmail.com>
2019-08-19 04:25:51 +0300

    New changes

---
+++ Controllers/DepartmentsController.cs
@@ -0,0 +1,64 @@
+using System.Collections.Generic;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using mis.Models;
+namespace mis.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class DepartmentsController : ControllerBase
+    {
+      private readonly DatabaseContext _context;
+      public DepartmentsController(DatabaseContext context) => _context = context;
+      //GET             api/departments
+      [HttpGet]
+      public ActionResult<IEnumerable<Departments>> GetDepartments()
+      {
+          return _context.Departments;
+      } 
+      //GET INDIVIDUAL CLIENTS      api/departments/id
+      [HttpGet("{id}")] 
+      public ActionResult<Departments> GetIndividualClient(int id)
+      {
+          var department = _context.Departments.Find(id);
+          if(department == null)
+          {
+              return NotFound();
+          }
+          return department;
+      }
+        //POST DEPARTMENTS             api/departments
+        [HttpPost]
+        public ActionResult<IEnumerable<Departments>> PostDepartments(Departments department)
+        {
+            _context.Departments.Add(department);
+            _context.SaveChanges();
+            return CreatedAtAction("GetDepartments", new Departments{id=department.id},department);
+        }
+        //PUT DEPARTMENTS       api/departments/id
+        [HttpPut("{id}")]
+        public ActionResult PutDepartment(int id, Departments department)
+        {
+            if(id != department.id)
+            {
+                return BadRequest();
+            }
+            _context.Entry(department).State = EntityState.Modified;
+            _context.SaveChanges();
+            return NoContent();
+        }
+        //DELETE DEPARTMENTS        api/departments/id
+        [HttpDelete("{id}")]
+        public ActionResult<Departments> DeleteDepartments(int id)
+        {
+            var department = _context.Departments.Find(id);
+            if(department == null)
+            {
+                return NotFound();
+            }
+            _context.Departments.Remove(department);
+            _context.SaveChanges();
+            return department;
+        }
+    }
+}
