f919c52144fcc97fba37cf660b235ba69c2810d0
Muuyi Andrew <muuyiandrew2015@gmail.com>
2019-10-02 17:26:17 +0300

    Updated

---
+++ Controllers/RoleController.cs
@@ -0,0 +1,15 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using Microsoft.AspNetCore.Identity;
+
+namespace mis.Controllers
+{
+    public class RoleController : ApiController
+    {
+        
+    }
+}
